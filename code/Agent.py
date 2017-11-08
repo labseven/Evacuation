a8394fe11c487b2629d5d4fabeb311470c34014a
Adam Novotny <adam.novotny@postrock.wall>
2017-11-08 14:12:53 -0500

    added framework

--- code/Agent.py
+++ code/Agent.py
@@ -1,7 +1,17 @@
 class Agent():
-	def __init__(self, size, mass):
+	def __init__(self, size, mass, pos, goal, desiredSpeed = 4):
 		"""
 		"""
 		self.size = size
 		self.mass = mass
-		self.velocity = 0
+		self.pos = pos
+		self.velocity = Point(0,0)
+		self.desiredSpeed = desiredSpeed
+		self.goal = goal
+
+class Point():
+	__init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	# TODO: Addition subtraction
--- code/Environment.py
+++ code/Environment.py
@@ -2,9 +2,25 @@ import pygame
 from pygame.locals import *
 from pygame.color import *
 
+class Wall():
+	def __init__(self, **parameters):
+		# type: "circle" | "line", points
+		self.parameters = parameters
+
+class Exit(Wall):
+	pass
+
 class Environment():
-	def __init__(self, N=100, wall, conditions):
+	self.conditions = { 'k': 1.2 * 10**5, 'ka': 2.4 * 10**5 }
+
+	def __init__(self, N=100, walls, exits, agents, conditions):
 		self.N = N
+		self.walls = walls
+		self.exits = exits
+		self.agents = agents
+		# Conditions: Agent force, Agent repulsive distance, acceleration time, step length,
+		self.conditions.update(conditions)
+
 
 	def step(self):
 		raise NotImplementedError
