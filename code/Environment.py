f68429bc2f4b6000265b0978cf1ead4c6ed19aaf
Adam Novotny <adam.novotny@postrock.wall>
2017-11-08 13:46:28 -0500

    added EnvironmentViewer

--- code/Environment.py
+++ code/Environment.py
@@ -1,6 +1,18 @@
+import pygame
+from pygame.locals import *
+from pygame.color import *
+
 class Environment():
 	def __init__(self, N=100, wall, conditions):
 		self.N = N
-		
-	def step():
+
+	def step(self):
+		raise NotImplementedError
+
+
+class EnvironmentViewer():
+	def __init__(self, environment):
+		self.environment = environment
+
+	def draw(self):
 		raise NotImplementedError
